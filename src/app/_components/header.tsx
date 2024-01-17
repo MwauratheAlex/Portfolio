8e8f939eca9052085a78b57fefd2f2db85dc6db9
Mbugua <mwaurathealex@gmail.com>
2024-01-17 18:01:04 +0300

    ui

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -52,8 +52,11 @@ const Header = () => {
                 className="md:ml-12"
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
               >
-                <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
-                <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Sun
+                  color="#d17a00"
+                  className="absolute h-6 w-6 rotate-90 scale-0  transition-all dark:rotate-0 dark:scale-100"
+                />
+                <Moon className="h-6 w-6 rotate-0 scale-100  transition-all dark:rotate-90 dark:scale-0" />
                 <span className="sr-only">Toggle Theme</span>
               </Button>
               <Drawer>
@@ -124,7 +127,7 @@ const SocialMedia = (props: { hidden: boolean }) => {
       </Button>
       <Button asChild variant="ghost" size="icon" aria-label="LinkedIn">
         <Link href="">
-          <FaLinkedin />
+          <FaLinkedin className="bg-red" />
           <span className="sr-only">XTwitter</span>
         </Link>
       </Button>
